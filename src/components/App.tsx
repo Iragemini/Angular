e929a9813100709f2a02202cad2d6c2ded8ba1ab
Iragemini <iragemini@gmail.com>
2021-02-27 15:34:34 +0300

    add eslint and prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import '../styles/index.scss';
 
-
 const App = () => {
-    return (
-        <>
-            <section className="hero"></section>
-            <main>
-                <section>
-                    <h1>Hi, React!</h1>
-                </section>
-            </main>
-        </>
-    )
-}
- export default App;
+  return (
+    <>
+      <section className="hero"></section>
+      <main>
+        <section>
+          <h1>Hi, React!</h1>
+        </section>
+      </main>
+    </>
+  );
+};
+export default App;
